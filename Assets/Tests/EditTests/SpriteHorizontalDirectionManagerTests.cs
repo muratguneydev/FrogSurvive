5a80b54c9bc1647ee9b336b672703e58538cdc8f
murat@mediasupreme.com <murat@mediasupreme.com>
2023-04-01 10:51:03 +0100

    Added more unit tests for SpriteHorizontalDirectionManager.

--- Assets/Tests/EditTests/SpriteHorizontalDirectionManagerTests.cs
+++ Assets/Tests/EditTests/SpriteHorizontalDirectionManagerTests.cs
@@ -1,25 +1,61 @@
-using System;
+using FrogSurvive.Events;
 using FrogSurvive.FrogPlayer;
 using NUnit.Framework;
+using UnityEngine;
 
 public class SpriteHorizontalDirectionManagerTests
 {
-	// [Test]
-    // public void Should_TurnRight_WhenRightKeyDown()
-	// {
-	// 	//Arrange
-	// 	var sut = GetSut(new RightHorizontalInputStub());
-	// 	var gameObject = TestGameObject.GetNew();
-	// 	//Act
-	// 	sut.ChangeDirection(rigidBody);
-	// 	//Assert
-	// 	//Assert.IsTrue(rigidBody.GetX() > originalX);
-	// 	Assert.AreEqual(rigidBody.velocity, Vector2.right * VelocityMoveUnitsPerSecond);
+	private static readonly SpriteHorizontalDirectionManager Sut = new();
 
-	// }
+	[Test]
+    public void Should_KeepFacingRight_WhenDirectionIsRight()
+	{
+		//Arrange
+		var gameObject = TestGameObject.GetNew();
+		gameObject.transform.localScale = new Vector3(2, 2, 2);
+		var signal = new FrogPlayerMovedSignal(gameObject, Vector2.right);
+		//Act
+		Sut.FrogPlayerMoved(signal);
+		//Assert
+		Assert.AreEqual(gameObject.transform.localScale, new Vector3(2, 2, 2));
+	}
 
-	// private SpriteHorizontalDirectionManager GetSut(RightHorizontalInputStub rightHorizontalInputStub)
-	// {
-	// 	throw new NotImplementedException();
-	// }
+	[Test]
+    public void Should_KeepFacingLeft_WhenDirectionIsLeft()
+	{
+		//Arrange
+		var gameObject = TestGameObject.GetNew();
+		gameObject.transform.localScale = new Vector3(-2, 2, 2);
+		var signal = new FrogPlayerMovedSignal(gameObject, Vector2.left);
+		//Act
+		Sut.FrogPlayerMoved(signal);
+		//Assert
+		Assert.AreEqual(gameObject.transform.localScale, new Vector3(-2, 2, 2));
+	}
+	
+	[Test]
+    public void Should_StartFacingLeft_WhenDirectionChangedFromRightToLeft()
+	{
+		//Arrange
+		var gameObject = TestGameObject.GetNew();
+		gameObject.transform.localScale = new Vector3(2, 2, 2);
+		var signal = new FrogPlayerMovedSignal(gameObject, Vector2.left);
+		//Act
+		Sut.FrogPlayerMoved(signal);
+		//Assert
+		Assert.AreEqual(gameObject.transform.localScale, new Vector3(-2, 2, 2));
+	}
+
+	[Test]
+    public void Should_StartFacingRight_WhenDirectionChangedFromLeftToRight()
+	{
+		//Arrange
+		var gameObject = TestGameObject.GetNew();
+		gameObject.transform.localScale = new Vector3(-2, 2, 2);
+		var signal = new FrogPlayerMovedSignal(gameObject, Vector2.right);
+		//Act
+		Sut.FrogPlayerMoved(signal);
+		//Assert
+		Assert.AreEqual(gameObject.transform.localScale, new Vector3(2, 2, 2));
+	}
 }
